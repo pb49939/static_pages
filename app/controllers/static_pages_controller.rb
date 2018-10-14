992eaa0b1fb6bc0b7c53c6f0a312f5c940c233d1
Patrick <prbaldwin@oakland.edu>
2018-10-14 05:34:05 -0400

    added tests and embedded ruby to make static pages somewhat dynamic

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,21 @@
+class StaticPagesController < ApplicationController
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
+  def home
+    
+  end
+
+  def help
+    
+  end
+
+  def about
+    
+  end
+
+  def contact 
+
+  end
+end
